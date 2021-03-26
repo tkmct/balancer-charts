e0009a1ca77627b24ad44343e8cd1b130a600041
tkmct <1220t.takamichi@gmail.com>
2021-03-27 03:49:27 +0900

    Style loading view

--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -1,3 +1,9 @@
-const LoadingIndicator = () => <div>Loading...</div>
+import styles from '../styles/components/LoadingIndicator.module.css'
+
+const LoadingIndicator = () => (
+  <div className={styles.container}>
+    <div className={styles.loading_text}>Loading...</div>
+  </div>
+)
 
 export default LoadingIndicator
